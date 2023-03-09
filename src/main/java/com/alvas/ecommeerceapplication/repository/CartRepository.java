5e63f3a4de253aa588604fbc013f435453540f36
Manoj Kumar M K <114489075+Manoj2208@users.noreply.github.com>
2023-03-09 15:17:35 +0530

    Update CartRepository.java

--- src/main/java/com/alvas/ecommeerceapplication/repository/CartRepository.java
+++ src/main/java/com/alvas/ecommeerceapplication/repository/CartRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.alvas.ecommeerceapplication.entity.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+  Cart findByCartIdAndUserUserId(long cartId,long userId);
 
 }
