2b71a006f56028f573050ac5272c7d17491e9f02
Manoj Kumar M K <114489075+Manoj2208@users.noreply.github.com>
2023-03-09 15:18:09 +0530

    Update WalletRepository.java

--- src/main/java/com/alvas/ecommeerceapplication/repository/WalletRepository.java
+++ src/main/java/com/alvas/ecommeerceapplication/repository/WalletRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.alvas.ecommeerceapplication.entity.Wallet;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+  Wallet findByWalletIdAndUserUserId(long walletId,long userId);
 
 }
