7d9bd039c41d95f3306616d9a56af92cdf2afb6d
vamsha591 <65599757+vamsha591@users.noreply.github.com>
2023-03-09 09:14:23 +0530

    Update UserNotFoundException.java

--- src/main/java/com/alvas/ecommeerceapplication/exception/UserNotFoundException.java
+++ src/main/java/com/alvas/ecommeerceapplication/exception/UserNotFoundException.java
@@ -7,15 +7,6 @@ public class UserNotFoundException extends RuntimeException {
 	 */
 	private static final long serialVersionUID = 1L;
 
-
-	public UserNotFoundException(String message) {
-		super(message);
-	}
-
-	public UserNotFoundException() {
-		super();
-	}
-
 	public UserNotFoundException(String message) {
 		super(message);
 	}
